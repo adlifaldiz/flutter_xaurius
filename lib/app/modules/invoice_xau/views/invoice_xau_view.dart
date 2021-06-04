0ae388baff677ac78b916f89c108a8bdf44ff24b
adlifaldiz <adlifaldizulfikasani@gmail.com>
2021-06-04 14:57:06 +0700

    update invoice

--- lib/app/modules/invoice_xau/views/invoice_xau_view.dart
+++ lib/app/modules/invoice_xau/views/invoice_xau_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_xaurius/helper/screen_utils.dart';
 import 'package:flutter_xaurius/helper/theme.dart';
+import 'package:flutter_xaurius/widget/xau_container.dart';
 
 import 'package:get/get.dart';
 import 'package:progress_indicators/progress_indicators.dart';
@@ -71,7 +72,219 @@ class InvoiceXauView extends GetView<InvoiceXauController> {
                       ],
                     )
                   ],
-                )
+                ),
+                SizedBox(height: 20),
+                XauriusContainer(
+                  child: Column(
+                    children: [
+                      Text(
+                        'Unit',
+                        style: textTitle,
+                      ),
+                      // SizedBox(height: 20),
+                      Divider(thickness: 2, height: 30, color: primaryColor),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Buy Unit Price',
+                            style: textTitle,
+                          ),
+                          Text(_invoiceController.responseDetailInvoice.value.data.buy.buyUnitPrice),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Network',
+                            style: textTitle,
+                          ),
+                          Text(_invoiceController.responseDetailInvoice.value.data.buy.buyNetwork),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Kuantitas',
+                            style: textTitle,
+                          ),
+                          Text(_invoiceController.responseDetailInvoice.value.data.buy.buyQty + ' XAU'),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Sub total',
+                            style: textTitle,
+                          ),
+                          Text(_invoiceController.responseDetailInvoice.value.data.buy.buyAmount + ' IDR'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                XauriusContainer(
+                  child: Column(
+                    children: [
+                      Text(
+                        'Descrption',
+                        style: textTitle,
+                      ),
+                      // SizedBox(height: 20),
+                      Divider(thickness: 2, height: 30, color: primaryColor),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Discount',
+                            style: textTitle,
+                          ),
+                          Text(_invoiceController.responseDetailInvoice.value.data.invoice.invoiceDiscount + ' IDR'),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Biaya admin',
+                            style: textTitle,
+                          ),
+                          Text(_invoiceController.responseDetailInvoice.value.data.invoice.invoiceAdmfee + ' IDR'),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Biaya GAS',
+                            style: textTitle,
+                          ),
+                          Text(_invoiceController.responseDetailInvoice.value.data.invoice.invoiceGas + ' IDR'),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Voucher',
+                            style: textTitle,
+                          ),
+                          Text(_invoiceController.responseDetailInvoice.value.data.invoice.invoiceVoucherValue + ' IDR'),
+                        ],
+                      ),
+                      // SizedBox(height: 20),
+                      Divider(thickness: 2, height: 30, color: primaryColor),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Total',
+                            style: textTitle,
+                          ),
+                          Text(_invoiceController.responseDetailInvoice.value.data.invoice.invoiceTotal.toString() + ' IDR'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                XauriusContainer(
+                  child: Column(
+                    children: [
+                      Text(
+                        'Informasi Pembayaran',
+                        style: textTitle,
+                      ),
+                      // SizedBox(height: 20),
+                      Divider(thickness: 2, height: 30, color: primaryColor),
+
+                      // Row(
+                      //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      //   children: [
+                      //     Text(
+                      //       'Nama Bank',
+                      //       style: textTitle,
+                      //     ),
+                      //     Text(_invoiceController.responseDetailInvoice.value.data.buy.buyAddress),
+                      //   ],
+                      // ),
+                      // SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Nomor Akun',
+                            style: textTitle,
+                          ),
+                          Text(_invoiceController.responseDetailInvoice.value.data.invoice.invoiceVa.vaNumber),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Nama Akun',
+                            style: textTitle,
+                          ),
+                          Text(_invoiceController.responseDetailInvoice.value.data.invoice.invoiceVa.customerData.custName),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Total',
+                            style: textTitle,
+                          ),
+                          Text(_invoiceController.responseDetailInvoice.value.data.invoice.invoiceTotal),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Bayar sebelum',
+                            style: textTitle,
+                          ),
+                          Text(_invoiceController.responseDetailInvoice.value.data.invoice.invoiceVa.vaExpiryDate.toString()),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                RaisedButton(
+                  onPressed: () {
+                    Get.focusScope.unfocus();
+                  },
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                  color: primaryColor,
+                  child: Container(
+                    width: percentWidth(context, 100),
+                    child: Center(
+                      child: Text(
+                        'Saya sudah bayar',
+                        style: buttonStyle,
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
