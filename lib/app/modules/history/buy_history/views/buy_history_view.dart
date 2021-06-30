fa11b1a1de737dcbb02147d9cc76e1d57311dcef
adlifaldiz <adlifaldizulfikasani@gmail.com>
2021-06-30 09:35:12 +0700

    resolve pagination loading widget
    done

--- lib/app/modules/history/buy_history/views/buy_history_view.dart
+++ lib/app/modules/history/buy_history/views/buy_history_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_xaurius/app/helpers/intl_formats.dart';
 import 'package:flutter_xaurius/app/helpers/screen_utils.dart';
@@ -9,7 +10,6 @@ import 'package:flutter_xaurius/app/widget/xau_container.dart';
 
 import 'package:get/get.dart';
 import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
-import 'package:progress_indicators/progress_indicators.dart';
 
 import '../controllers/buy_history_controller.dart';
 
@@ -24,8 +24,11 @@ class BuyHistoryView extends GetView<BuyHistoryController> {
       showChildOpacityTransition: false,
       child: Obx(
         () {
-          if (controller.isLoading.value && controller.totalRecords.value == 0) {
-            return ShimmerList();
+          if (controller.isLoading.value && controller.listBuys.length == 0) {
+            return Padding(
+              padding: EdgeInsets.symmetric(vertical: percentHeight(context, 1)),
+              child: ShimmerList(),
+            );
           }
           if (controller.listBuys.isEmpty) {
             return EmptyState(
@@ -34,78 +37,90 @@ class BuyHistoryView extends GetView<BuyHistoryController> {
             );
           }
 
-          return ListView.builder(
-            padding: EdgeInsets.symmetric(horizontal: percentWidth(context, 5), vertical: percentHeight(context, 1)),
-            controller: controller.scrollController,
-            itemCount: controller.listBuys.length,
-            itemBuilder: (context, index) {
-              if (index == controller.listBuys.length - 1 && controller.isLoading.value) {
-                return Center(child: JumpingDotsProgressIndicator(fontSize: 40, color: primaryColor));
-              }
-              return Padding(
-                padding: EdgeInsets.symmetric(vertical: percentWidth(context, 1)),
-                child: GestureDetector(
-                  onTap: () => Get.toNamed(Routes.INVOICE, arguments: {
-                    'invoiceId': controller.listBuys[index].invoiceId,
-                    'fromBuy': false,
-                  }),
-                  child: XauriusContainer(
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Text(
-                                  'Invoice',
-                                  style: textTitle,
-                                ),
-                                SizedBox(width: 5),
-                                Text(
-                                  '#' + controller.listBuys[index].invoiceId.toString(),
-                                ),
-                              ],
-                            ),
-                            Text(
-                              controller.listBuys[index].buyStatus.toString(),
-                              style: textTitle.copyWith(color: primaryColor),
-                            ),
-                          ],
+          return Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  padding: EdgeInsets.symmetric(horizontal: percentWidth(context, 5), vertical: percentHeight(context, 1)),
+                  controller: controller.scrollController,
+                  itemCount: controller.listBuys.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.symmetric(vertical: percentWidth(context, 1)),
+                      child: GestureDetector(
+                        onTap: () => Get.toNamed(Routes.INVOICE, arguments: {
+                          'invoiceId': controller.listBuys[index].invoiceId,
+                          'fromBuy': false,
+                        }),
+                        child: XauriusContainer(
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Text(
+                                        'Invoice',
+                                        style: textTitle,
+                                      ),
+                                      SizedBox(width: 5),
+                                      Text(
+                                        '#' + controller.listBuys[index].invoiceId.toString(),
+                                      ),
+                                    ],
+                                  ),
+                                  Text(
+                                    controller.listBuys[index].buyStatus.toString(),
+                                    style: textTitle.copyWith(color: primaryColor),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 20),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'quantity_xau'.tr,
+                                    style: stylePrimary,
+                                  ),
+                                  Text(
+                                    controller.listBuys[index].buyQty + ' XAU',
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 10),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Total ',
+                                    style: stylePrimary,
+                                  ),
+                                  Text(
+                                    customCurrency(controller.listBuys[index].buyAmount) + ' IDR',
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 10),
+                            ],
+                          ),
                         ),
-                        SizedBox(height: 20),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'quantity_xau'.tr,
-                              style: stylePrimary,
-                            ),
-                            Text(
-                              controller.listBuys[index].buyQty + ' XAU',
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 10),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'Total ',
-                              style: stylePrimary,
-                            ),
-                            Text(
-                              customCurrency(controller.listBuys[index].buyAmount) + ' IDR',
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 10),
-                      ],
-                    ),
-                  ),
+                      ),
+                    );
+                  },
                 ),
-              );
-            },
+              ),
+              AnimatedContainer(
+                height: controller.isLoading.value ? percentHeight(context, 2) : 0,
+                duration: Duration(milliseconds: 100),
+                child: controller.isLoading.value
+                    ? (GetPlatform.isIOS || GetPlatform.isMacOS)
+                        ? CupertinoActivityIndicator()
+                        : CircularProgressIndicator(color: primaryColor, strokeWidth: 1.5)
+                    : Container(),
+              )
+            ],
           );
         },
       ),
