4a5894e51fa23e706b2f107f20a953a5235b7561
BramantioADP <Bramtioo64@gmail.com>
2021-07-05 15:21:34 +0700

    deposit tampilan & min sdk up 20
    because of qr scan the minum sdk needed to upped to 20 or more
    and update deposit view thanks to adli fadli

--- lib/app/modules/deposit/controllers/deposit_controller.dart
+++ lib/app/modules/deposit/controllers/deposit_controller.dart
@@ -1,9 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_xaurius/app/data/model/balances/balance_data.dart';
+import 'package:flutter_xaurius/app/helpers/text_controller_utils.dart';
+import 'package:flutter_xaurius/app/modules/dashboard/controllers/dashboard_controller.dart';
 import 'package:get/get.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class DepositController extends GetxController {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  TextEditingController linkText = TextEditingController();
+  List balanceDummy = <BalanceData>[
+    BalanceData(balanceSymbol: "XAU", balanceValue: "1"),
+    BalanceData(balanceSymbol: "IDR", balanceValue: "10000"),
+  ];
+
+  QRViewController qrController;
+  var scanArea = (Get.width < 400 || Get.height < 400) ? 150.0 : 300.0;
+  var dash = Get.find<DashboardController>();
+  var balance = <BalanceData>[].obs;
+  var address =
+      'https://www.google.com/search?q=how+to+see+japanese+tiktok&safe=strict&client=firefox-b-d&sxsrf=ALeKk0045JlWLCz7pG1JMbORWLGdft1TEg%3A1625397089023&ei=YZfhYIhjzb2tAYW7taAC&oq=how+to+see+japa&gs_lcp=Cgdnd3Mtd2l6EAMYATICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyBggAEBYQHjIGCAAQFhAeMgYIABAWEB46BwgAEEcQsAM6BQgAEJECOggIABCxAxCDAToICC4QsQMQgwE6BQgAELEDOgsILhCxAxDHARCjAjoECCMQJzoECAAQQzoHCC4QQxCTAjoECAAQCjoHCAAQhwIQFDoCCC5KBAhBGABQopRFWMr5RWCwi0ZoB3ACeACAAa8BiAHYDZIBBDEzLjWYAQCgAQGqAQdnd3Mtd2l6yAEIwAEB&sclient=gws-wiz'
+          .obs;
+
+  TextEditingController addressController, xauController;
+  NumericTextController idrController;
+
   @override
-  void onInit() {
-    // TODO: implement onInit
+  Future onInit() async {
+    setTextController();
+    balance.value = dash.balance;
+    linkText.text = address.toString();
     super.onInit();
   }
 
@@ -17,5 +42,74 @@ class DepositController extends GetxController {
   void onClose() {
     // TODO: implement onClose
     super.onClose();
+  }
+
+  @override
+  void dispose() {
+    qrController.dispose();
+  }
+
+  void setTextController() {
+    addressController = TextEditingController();
+    xauController = TextEditingController();
+    idrController = NumericTextController();
+  }
+
+  Future onQRViewCreated(QRViewController qrController) async {
+    this.qrController = qrController;
+    qrController.scannedDataStream.take(1).listen((scanData) {
+      String s = scanData.code.substring(scanData.code.indexOf(':') + 1);
+      addressController.text = s.trim();
+      Get.back();
+    });
+  }
+
+  onQtyChange(val) {
+    if (val.isEmpty) {
+      xauController.text = '';
+      xauController.selection = TextSelection.fromPosition(
+          TextPosition(offset: xauController.text.length));
+    } else if (val[0] == '.') {
+      xauController.text = '0.';
+      xauController.selection = TextSelection.fromPosition(
+          TextPosition(offset: xauController.text.length));
+    } else if (val[0] == '0' && val[1].toString().isNum) {
+      xauController.text = '0.';
+      xauController.selection = TextSelection.fromPosition(
+          TextPosition(offset: xauController.text.length));
+    } else if (val.contains(',')) {
+      xauController.text = val.replaceAll(',', '.');
+      xauController.selection = TextSelection.fromPosition(
+          TextPosition(offset: xauController.text.length));
+    } else {
+      // var value = double.parse(val);
+      // var subtotal = double.parse(dash.goldPrice.value.chartpriceBuy) * value;
+      // var total = subtotal.toInt();
+      // idrController.text = total.toString();
+    }
+  }
+
+  onTotalChange(val) {
+    val = idrController.numberValue;
+
+    if (val.toString().isEmpty) {
+      idrController.text = '';
+      idrController.selection = TextSelection.fromPosition(
+          TextPosition(offset: idrController.text.length));
+    }
+    // else if (val[0] == '.') {
+    //   idrController.text = '5';
+    //   idrController.selection = TextSelection.fromPosition(TextPosition(offset: qtyController.text.length));
+    // } else if (val.contains('..')) {
+    //   idrController.text = val.replaceAll(RegExp('..'), '');
+    //   idrController.selection = TextSelection.fromPosition(TextPosition(offset: idrController.text.length));
+    // } else if (val.contains(',')) {
+    //   idrController.text = val.replaceAll(RegExp(','), '');
+    //   idrController.selection = TextSelection.fromPosition(TextPosition(offset: idrController.text.length));
+    // }
+    else {
+      // var total = val / double.parse(dash.goldPrice.value.chartpriceBuy);
+      // xauController.text = total.toString();
+    }
   }
 }
--- lib/app/modules/deposit/views/deposit_view.dart
+++ lib/app/modules/deposit/views/deposit_view.dart
@@ -1,28 +1,175 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_xaurius/app/helpers/screen_utils.dart';
+import 'package:flutter_xaurius/app/helpers/theme.dart';
 
 import 'package:get/get.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:flutter/services.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 import '../controllers/deposit_controller.dart';
 
 class DepositView extends GetView<DepositController> {
   @override
   Widget build(BuildContext context) {
+    var x = Get.width * 0.1;
     return GestureDetector(
       onTap: () => Get.focusScope.unfocus(),
       child: Scaffold(
         appBar: AppBar(
           title: Text('Deposit Crypto'),
         ),
-        body: Container(
-          width: Get.width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [Text('This is Deposit View')],
+        body: Obx(
+          () => SafeArea(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.symmetric(
+                  horizontal: percentWidth(context, 5),
+                  vertical: percentHeight(context, 2)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ListView(
+                    shrinkWrap: true,
+                    children: controller.balanceDummy
+                        .where(
+                            (itemBalance) => itemBalance.balanceSymbol == "XAU")
+                        .map((itemBalance) {
+                      return Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: percentWidth(context, 5),
+                            vertical: percentHeight(context, 3)),
+                        width: Get.width,
+                        decoration: BoxDecoration(
+                          color: backgroundPanelColor.withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        foregroundDecoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            image: DecorationImage(
+                                alignment: Alignment.bottomRight,
+                                image: AssetImage(
+                                  'assets/images/mesh-bottom.png',
+                                ))),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              itemBalance.balanceSymbol,
+                              style: textTitle.copyWith(color: primaryColor),
+                            ),
+                            SizedBox(height: 5),
+                            Text(
+                              itemBalance.balanceValue,
+                              style: textTitle.copyWith(fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                  SizedBox(height: percentHeight(context, 5)),
+                  Center(
+                      child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            border: Border.all(
+                              width: Get.width * 0.1,
+                              color: backgroundPanelColor.withOpacity(0.1),
+                            ),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Container(
+                              child: QrImage(
+                            data: controller.address.toString(),
+                            version: QrVersions.auto,
+                            size: Get.width * 0.5,
+                            gapless: false,
+                            backgroundColor: Colors.white,
+                          )))),
+                  SizedBox(height: percentHeight(context, 4)),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                            width: Get.width * 0.8,
+                            child: TextField(
+                              readOnly: true,
+                              controller: controller.linkText,
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Colors.white10,
+                              ),
+                            )),
+                        // Expanded(
+                        //     child: ButtonTheme(
+                        //         minWidth: x,
+                        //            shape: RoundedRectangleBorder(
+                        //                borderRadius: BorderRadius.circular(5)),
+                        //         height: x,
+                        //         child: Center(
+                        //             child: RaisedButton(
+                        //                 onPressed: () => Clipboard.setData(
+                        //                     ClipboardData(
+                        //                         text: controller.address
+                        //                             .toString())),
+                        //                 padding: EdgeInsets.zero,
+                        //                 child: Center(
+                        //                     child: Icon(
+                        //                   Icons.content_copy,
+                        //                   color: Colors.white,
+                        //                 ))))))
+                      ]),
+                  SizedBox(height: percentHeight(context, 2)),
+                  RaisedButton(
+                    onPressed: () => Clipboard.setData(
+                        ClipboardData(text: controller.address.toString())),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    color: primaryColor,
+                    child: Center(
+                      child: Text(
+                        'Copy Adress',
+                        style: buttonStyle,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  _showQrScan(BuildContext context) {
+    showCupertinoModalBottomSheet(
+      isDismissible: true,
+      context: context,
+      builder: (context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('QR Scanner'),
+          ),
+          body: Stack(
+            children: [
+              QRView(
+                key: controller.qrKey,
+                onQRViewCreated: controller.onQRViewCreated,
+                overlay: QrScannerOverlayShape(
+                    borderColor: Colors.red,
+                    borderRadius: 10,
+                    borderLength: 30,
+                    borderWidth: 10,
+                    cutOutSize: controller.scanArea),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
