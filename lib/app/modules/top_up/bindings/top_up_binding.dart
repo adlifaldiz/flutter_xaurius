cd1d5a4ade12b11dbc1da1edad4bb1dbbc613dbe
BramantioADP <Bramtioo64@gmail.com>
2021-07-12 14:03:39 +0700

    ui/ux and stuff
    small change to ui ux and stuff

--- lib/app/modules/top_up/bindings/top_up_binding.dart
+++ lib/app/modules/top_up/bindings/top_up_binding.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_xaurius/app/modules/history/topup_history/controllers/topup_history_controller.dart';
 import 'package:get/get.dart';
 
 import '../controllers/top_up_controller.dart';
@@ -7,6 +8,9 @@ class TopUpBinding extends Bindings {
   void dependencies() {
     Get.lazyPut<TopUpController>(
       () => TopUpController(),
+    );
+    Get.lazyPut<TopupHistoryController>(
+      () => TopupHistoryController(),
     );
   }
 }
